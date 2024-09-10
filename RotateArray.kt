dae84691aeed3c79b7a71cf0b9470f380c04d50a
uitangclan <uitangclan@gmail.com>
2024-09-10 21:39:26 +0900

    happy coding

---
+++ RotateArray.kt
@@ -0,0 +1,11 @@
+fun main (){
+    val nums = intArrayOf(1,2,3,4,5,6,7)
+    val k = 3
+    // val nums = intArrayOf(-1,-100,3,99)
+    // val k = 2
+    println(rotate(nums, k))
+}
+
+fun rotate(nums: IntArray, k: Int): Unit {
+
+}
