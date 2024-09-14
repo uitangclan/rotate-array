03d709a50b59bb7be6c8e3b4cdce4d03c0bed25f
uitangclan <uitangclan@gmail.com>
2024-09-14 13:37:14 +0900

    happy coding

--- RotateArray.kt
+++ RotateArray.kt
@@ -1,20 +1,19 @@
 fun main (){
-    var nums = intArrayOf(1,2,3,4,5,6,7)
-    var k = 3
-    // var nums = intArrayOf(1,2,3,4)
-    // var k = 2
+    // var nums = intArrayOf(1,2,3,4,5,6,7)
+    // var k = 3
+    var nums = intArrayOf(1,2,3,4)
+    var k = 2
     // val nums = intArrayOf(-1,-100,3,99)
     // val k = 2
     println(rotate(nums, k))
 }
 
 fun rotate(nums: IntArray, k: Int): Unit {
-    for (i in 1..k) {
-        var this = nums[nums.size - 1]
-        for (j in nums.indices) {
-            var temp = nums[j]
-            nums[j] = this 
-            this = temp
-        }
+    var a = IntArray(nums.size) {0}
+    for (i in nums.indices) {
+        a[(i + k) % nums.size] = nums[i]
+    }
+    for (i in nums.indices) {
+            nums[i] = a[i]
     }
 }
